1a13af3603864833a4ddef975cfd688e442ff16b
twattley <wattley83@gmail.com>
2026-02-13 08:19:29 +0000

    wuip

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5175,
+    port: 5176,
     proxy: {
       "/api": {
-        target: "http://localhost:8002",
+        target: "http://localhost:8003",
         changeOrigin: true,
       },
     },
